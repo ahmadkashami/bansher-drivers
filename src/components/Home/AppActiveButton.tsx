0d6d2d28177aad82267acd98a07bc507f862b893
bansher01 <bansher01@gmail.com>
2022-11-13 01:24:32 +0300

    add tab bottom navigator and profile screen

---
+++ src/components/Home/AppActiveButton.tsx
@@ -0,0 +1,48 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { AppColors } from "../../contants/Colors";
+
+const AppActiveButton = ({
+  disabled,
+  isActive,
+  onPress,
+}: {
+  disabled: boolean;
+  isActive: boolean;
+  onPress: () => void;
+}) => {
+  return (
+    <Pressable
+      disabled={disabled}
+      onPress={onPress}
+      style={({ pressed }) => [
+        styles.container,
+        pressed && styles.pressed,
+        isActive && { backgroundColor: "green" },
+      ]}
+    >
+      <Text style={styles.text}>{isActive ? "online" : "offline"}</Text>
+    </Pressable>
+  );
+};
+
+export default AppActiveButton;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gray",
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  text: {
+    color: AppColors.white,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  pressed: {
+    opacity: 0.6,
+  },
+});
